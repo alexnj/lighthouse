73b3387cb0271756223e6656f8c777d55c439b3f
Alex N. Jose <alexnj@google.com>
2022-08-25 15:40:12 -0700

    Moved the comment to jsDoc.

--- cli/test/smokehouse/config/config.js
+++ cli/test/smokehouse/config/config.js
@@ -4,10 +4,11 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-/** @type {Record<string, Array<string>>} */
+/**
+ * List of smoke tests excluded per runner. eg: 'cli': ['a11y', 'dbw']
+ * @type {Record<string, Array<string>>}
+ */
 const exclusions = {
-  // runner: ['excluded1', 'excluded2']
-  // e.g. 'cli': ['a11y']
   'cli': [],
   'devtools': [
     // Disabled because normal Chrome usage makes DevTools not function on
